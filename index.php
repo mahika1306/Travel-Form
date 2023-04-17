c3fc69e6cc270bdb0c22fec5c7f63b52238689e7
Mahika Dhar M <117369643+mahika1306@users.noreply.github.com>
2023-04-17 12:47:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+$insert=false;
+if(isset($_POST['name'])){
+     $server="localhost";
+     $username="root";
+     $password="";
+
+      $con=mysqli_connect($server,$username,$password);
+
+      if(!$con){
+        die("Connection to this database failed due to".mysqli_connect_error());
+      }
+      //echo "Success connecting to db";
+     $name=$_POST['name'];
+     $gender=$_POST['gender'];
+     $age=$_POST['age'];
+     $email=$_POST['email'];
+     $phone=$_POST['phone'];
+     $desc=$_POST['desc'];
+
+      $sql="INSERT INTO `travels`.`trip` ( `name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+      //echo $sql;
+
+      if($con->query($sql) == true){
+        //echo "successfully inserted";
+        $insert=true;
+      }
+      else{
+        echo "ERROR: $sql <br> $conn->error";
+      }
+      $con->close();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Travel Form</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <img class="img" src="img.jpg" alt="img">
+    <div class="container">
+        <h1>Welcome</h1>
+        <p>Enter your details and confirm your participation in the trip</p>
+        <?php
+        if($insert==true){
+        echo "<p class='msg'>Thanks for submitting the form</p>";
+        }
+        ?>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="text" name="age" id="age" placeholder="Enter your age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your Gender">
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+            <input type="phone" name="phone" id="phone" placeholder="Enter your Phone number">
+            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any further information"></textarea>
+            <button class="btn">Submit</button>
+        </form>
+        
+    </div>
+    <script src=""index.js"></script>
+   
+</body>
+</html>
